453c0bdd16e4a5015139471b4444a1e733eae010
dm4t2 <dm4t2@outlook.com>
2019-10-16 22:36:33 +0200

    fix: add missing exports for UMD bundle
    closes #38

--- src/index.umd.js
+++ src/index.umd.js
@@ -1,4 +1,14 @@
+import { parseCurrency } from './api'
+import component from './component'
+import directive from './directive'
 import plugin from './plugin'
+
+export default plugin
+export {
+  parseCurrency,
+  component as CurrencyInput,
+  directive as CurrencyDirective
+}
 
 // Auto install when included directly in the browser
 if (typeof window !== 'undefined' && window.Vue) {
