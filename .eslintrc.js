9bf98a79256fccd8b8a8b6646daebf899be31645
dm4t2 <dm4t2@outlook.com>
2021-10-22 17:42:15 +0200

    chore: fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,32 +3,25 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "prettier/@typescript-eslint"],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'prettier/@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    "@typescript-eslint/ban-ts-comment": "off",
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    '@typescript-eslint/ban-ts-comment': 'off'
   },
+  ignorePatterns: ['dist/**'],
   overrides: [
     {
-      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true
       }
     },
     {
-      files: ["**/*.vue"],
-      extends: [
-        "plugin:vue/vue3-recommended",
-        "eslint:recommended",
-        "@vue/typescript/recommended",
-        "@vue/prettier",
-        "@vue/prettier/@typescript-eslint"
-      ],
-      plugins: ["vue"]
+      files: ['**/*.vue'],
+      extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', '@vue/typescript/recommended', '@vue/prettier', '@vue/prettier/@typescript-eslint'],
+      plugins: ['vue']
     }
   ]
-};
+}
