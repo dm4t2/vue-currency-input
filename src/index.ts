45559288eb312c0172e3005cd8aa2767a2bc9f18
dm4t2 <dm4t2@outlook.com>
2022-07-02 23:51:47 +0200

    feat: remove default export
    BREAKING CHANGE:
    The default export was removed. Use the named export `useCurrencyInput` instead.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import useCurrencyInput from './useCurrencyInput'
 
-/**
- * @deprecated Use the named export `useCurrencyInput` instead.
- */
-export default useCurrencyInput
 export * from './api'
-
 export { useCurrencyInput }
